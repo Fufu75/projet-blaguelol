ee74041d4262c7d73b26e1954534df4599dfca47
thomassena-95 <thomassena95@gmail.com>
2025-03-11 15:46:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import pyjokes
 
-print(pyjokes.get_joke())
+def tell_joke(lang="en"):
+    print(pyjokes.get_joke(language=lang))
 
+if __name__ == "__main__":
+    lang = input("Choisissez une langue (en, de, es, it, gl): ")
+    tell_joke(lang)
